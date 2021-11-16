e7edc7f02014300b492dd80766e5a78acc5abe55
Menchu MV <mcmvillalobos@gmail.com>
2021-11-16 22:18:59 +0100

    Added [id] page to ssr folder and linked it to index, fetching with getServerSideProps

--- pages/mypokemon-ssr/index.js
+++ pages/mypokemon-ssr/index.js
@@ -1,10 +1,19 @@
+import Link from "next/link";
+import styles from "../../styles/PokemonPages.module.css";
+
 const MyPokemonSSR = ({ pokemons }) => {
   return (
     <>
       <h1>My Pokemon SSR</h1>
       <ul>
         {pokemons.map((pokemon) => (
-          <li key={pokemon.name}>{pokemon.name}</li>
+          <Link
+            href={`/mypokemon-ssr/${pokemon.id}`}
+            key={pokemon.name}
+            passHref
+          >
+            <li className={styles.link}>{pokemon.name}</li>
+          </Link>
         ))}
       </ul>
     </>
