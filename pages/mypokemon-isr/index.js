f8ac736bb42ed23c56c78e080650bd100490395c
Menchu MV <mcmvillalobos@gmail.com>
2021-11-17 13:26:28 +0100

    Installed jest and created config files

--- pages/mypokemon-isr/index.js
+++ pages/mypokemon-isr/index.js
@@ -1,24 +1,18 @@
 import Link from "next/link";
 import styles from "../../styles/PokemonPages.module.css";
 
-const MyPokemonISR = ({ pokemons }) => {
-  return (
-    <>
-      <h1>My Pokemon ISR</h1>
-      <ul>
-        {pokemons.map((pokemon) => (
-          <Link
-            href={`/mypokemon-isr/${pokemon.id}`}
-            key={pokemon.name}
-            passHref
-          >
-            <li className={styles.link}>{pokemon.name}</li>
-          </Link>
-        ))}
-      </ul>
-    </>
-  );
-};
+const MyPokemonISR = ({ pokemons }) => (
+  <>
+    <h1>My Pokemon ISR</h1>
+    <ul>
+      {pokemons.map((pokemon) => (
+        <Link href={`/mypokemon-isr/${pokemon.id}`} key={pokemon.name} passHref>
+          <li className={styles.link}>{pokemon.name}</li>
+        </Link>
+      ))}
+    </ul>
+  </>
+);
 
 export async function getStaticProps() {
   const res = await fetch(`https://pokeapi-menchu.herokuapp.com/pokemon`);
--- pages/mypokemon-ssr/index.js
+++ pages/mypokemon-ssr/index.js
@@ -1,24 +1,18 @@
 import Link from "next/link";
 import styles from "../../styles/PokemonPages.module.css";
 
-const MyPokemonSSR = ({ pokemons }) => {
-  return (
-    <>
-      <h1>My Pokemon SSR</h1>
-      <ul>
-        {pokemons.map((pokemon) => (
-          <Link
-            href={`/mypokemon-ssr/${pokemon.id}`}
-            key={pokemon.name}
-            passHref
-          >
-            <li className={styles.link}>{pokemon.name}</li>
-          </Link>
-        ))}
-      </ul>
-    </>
-  );
-};
+const MyPokemonSSR = ({ pokemons }) => (
+  <>
+    <h1>My Pokemon SSR</h1>
+    <ul>
+      {pokemons.map((pokemon) => (
+        <Link href={`/mypokemon-ssr/${pokemon.id}`} key={pokemon.name} passHref>
+          <li className={styles.link}>{pokemon.name}</li>
+        </Link>
+      ))}
+    </ul>
+  </>
+);
 
 export async function getServerSideProps() {
   const res = await fetch(`https://pokeapi-menchu.herokuapp.com/pokemon`);
